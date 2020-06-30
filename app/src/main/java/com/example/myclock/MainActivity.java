66061d9746c2d046065e9a9a310b5858ea384549
양희범 <h6yang@naver.com>
2020-07-01 06:03:17 +0900

    extends를 implements로 수정, 실행 성공

--- app/src/main/java/com/example/myclock/MainActivity.java
+++ app/src/main/java/com/example/myclock/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        class newRunnable extends Runnable {
+        class newRunnable implements Runnable {
 
             public void run() {
                 while (true) {
