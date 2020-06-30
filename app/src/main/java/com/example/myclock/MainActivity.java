82b497bf993552137d3dec2b58090c944213728e
양희범 <h6yang@naver.com>
2020-07-01 05:26:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myclock/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    TextView clockTextView ;
+    private static Handler mHandler ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Calendar cal = Calendar.getInstance() ;
+
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                String strTime = sdf.format(cal.getTime());
+
+                clockTextView = findViewById(R.id.clock) ;
+                clockTextView.setText(strTime) ;
+            }
+        };
+
+        class NewRunnable implements Runnable {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace() ;
+                    }
+                    mHandler.sendEmptyMessage(0) ;
+                }
+            }
+        }
+
+        NewRunnable ne = new NewRunnable() ;
+        Thread t = new Thread(ne) ;
+        t.start() ;
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
